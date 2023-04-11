ff074f246a332f1cebe3c5ec826b752cab24f202
alto <alto.martin@outlook.com>
2023-04-11 15:34:20 +0800

    feat: modify board's show action for nested resources

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: %i[ show edit update destroy ]
+  before_action :set_posts, only: %i[ show ]
 
   # GET /boards or /boards.json
   def index
@@ -61,6 +62,10 @@ class BoardsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_board
       @board = Board.find(params[:id])
+    end
+
+    def set_posts
+      @posts = @board.posts
     end
 
     # Only allow a list of trusted parameters through.
