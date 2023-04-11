b132dd00659dcee00551b965e78b8491a9f91ba4
alto <alto.martin@outlook.com>
2023-04-11 14:01:31 +0800

    feat: scaffold board and post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :boards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#about"
 end
