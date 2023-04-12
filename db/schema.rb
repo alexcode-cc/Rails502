2638365ba1628fc6ffe75c1bd8c667486802eb34
alto <alto.martin@outlook.com>
2023-04-12 09:50:20 +0800

    feat: add username to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_005015) do
+ActiveRecord::Schema.define(version: 2023_04_12_012148) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 2023_04_12_005015) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
